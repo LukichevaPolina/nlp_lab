80a99240da241beaf29a67158f3ca5c9960dee29
Anton Klykov <antonklykov01@mail.ru>
2024-10-12 20:33:19 +0300

    received dataset

---
+++ src/utils/get_data.py
@@ -0,0 +1,6 @@
+import logging as log
+import pandas as pd
+
+def get_data(path: str="/content/drive/MyDrive/nlp_lab/Combined Data.csv") -> pd.DataFrame:
+    log.info(f"Get dataset from {path}")
+    return pd.read_csv(path).drop("Unnamed: 0", axis=1)
