5f46efef0a4c6e7e06d8266acd3d6c08314c1a1e
Anton Klykov <antonklykov01@mail.ru>
2024-10-23 23:32:16 +0300

    update

--- src/models/cnn/infer.py
+++ src/models/cnn/infer.py
@@ -3,12 +3,12 @@ from src.models.cnn.conv_psycho_net import ConvPsychoNet
 
 def cnn_infer(checkpoint_path, X):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    checkpoint = torch.load(checkpoint_path, map_location=device)
     psycho_net = ConvPsychoNet(1, 7).to(device)
-    psycho_net.load_state_dict(checkpoint)
+    psycho_net.load_state_dict(torch.load(checkpoint_path))
     psycho_net.eval()
 
     with torch.no_grad():
+        X = torch.from_numpy(X.toarray().reshape(-1, 14951)).float().unsqueeze(dim=0).unsqueeze(dim=1)
         pred = psycho_net(X)
 
         y_pred = torch.softmax(pred, dim=1).armax(dim=1)
